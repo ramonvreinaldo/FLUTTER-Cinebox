faf72bdedec8e82fd8375bdfa1230541f672c866
Ramon Vieira Reinaldo <ramon-r-reinaldo@alticelabs.com>
2025-08-24 20:44:58 +0100

    Riverpod set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
+import 'package:cinebox/cinebox_main_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Text('Ramon Vieira Reinaldo'),
-    );
-  }
+  runApp(const ProviderScope(child: CineboxMainApp()));
 }
---
+++ lib/ui/splash/splash_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SplashScreen extends ConsumerStatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome to CineBox',
+        ),
+      ),
+    );
+  }
+}
