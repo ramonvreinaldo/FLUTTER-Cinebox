42ed0f8e4e0a99ecddc5493bc127d5916dc83a83
Ramon Vieira Reinaldo <ramon-r-reinaldo@alticelabs.com>
2025-09-25 06:30:37 +0100

    015 - BackendAuthInterceptor

--- lib/data/core/rest_client/backend_rest_client.dart
+++ lib/data/core/rest_client/backend_rest_client.dart
@@ -1,8 +1,29 @@
 import 'package:cinebox/config/env.dart';
+import 'package:cinebox/core/result/result.dart';
+import 'package:cinebox/data/services/services_providers.dart';
 import 'package:dio/dio.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'backend_rest_client.g.dart';
+
+class BackendAuthInterceptor extends Interceptor {
+  final Ref ref;
+
+  BackendAuthInterceptor({required this.ref});
+
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final localStorage = ref.read(localStorageServiceProvider);
+    final idTokenResult = await localStorage.getIdToken();
+    if (idTokenResult case Success(value: final idToken)) {
+      options.headers['Authorization'] = 'Bearer $idToken';
+    }
+    handler.next(options);
+  }
+}
 
 @Riverpod(keepAlive: true)
 Dio backendRestClient(Ref ref) {
